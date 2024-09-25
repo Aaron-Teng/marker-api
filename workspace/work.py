d8ec4b9d349c3cbfc25ce89a8535526301f18897
Aaron-Teng <aaron_teng@hotmail.com>
2024-09-25 11:48:44 +0800

    补充文本日志

--- workspace/work.py
+++ workspace/work.py
@@ -2,6 +2,7 @@ from invoke import convert_pdf_to_markdown_and_save as convert
 import os
 import time
 import glob
+import logging
 
 inputs = [
     'a.pdf',
@@ -12,6 +13,14 @@ inputs = [
 inputs = ['./inputs/' + f for f in inputs]
 outputs = 'outputs'
 server_url = 'http://127.0.0.1:8000/convert'
+
+# 确保logs目录存在
+os.makedirs('logs', exist_ok=True)
+logging.basicConfig(
+    filename=f'logs/app_{time.strftime("%Y%m%d%H%M%S", time.localtime())}.log',  # 日志文件名
+    level=logging.DEBUG,  # 日志级别
+    format='%(asctime)s - %(levelname)s - %(message)s'  # 日志格式
+)
 
 
 def ymdhms(): return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
@@ -29,10 +38,13 @@ inputs = get_inputs_all_pdf()
 
 total = len(inputs)
 print(f'【开始】[{ymdhms()}] {total} files.')
+logging.info(f'【开始】[{ymdhms()}] {total} files.')
 start_time = time.time()
 
 for idx, pdf in enumerate(inputs):
     print(f'【{idx + 1}/{total}】 {os.path.basename(pdf)}')
+    logging.info(f'【{idx + 1}/{total}】 {os.path.basename(pdf)}')
     convert([pdf], outputs, server_url)
 
 print(f'【结束】[{ymdhms()}] {total} files. take {time.time() - start_time:.2f}s.')
+logging.info(f'【结束】[{ymdhms()}] {total} files. take {time.time() - start_time:.2f}s.')
